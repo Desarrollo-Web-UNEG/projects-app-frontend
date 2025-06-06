35d4b49d0ce47a06605df6e47ff0a601cd58ed94
Sebastián Landaeta <sebas.landa@hotmail.com>
2025-06-06 13:12:08 -0400

    [FIX] Mejoras en la vista
    - Se ajustaron los componentes visuales para que sus dimensiones se ajusten según el tamaño de pantalla y zoom.
    - Se agregaron los iconos dentro de los inputs.
    - Se agregó el subtítulo faltante.
    - Se cambió el tipo de fuente.
    - Se modificó el link de registro para que se viera como el del diseño definido.

--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -1,37 +1,56 @@
 import { Link } from "react-router-dom";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, InputAdornment } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
 import "../styles/login-form.css";
 
 const LoginForm = () => {
   return (
     <div className="login-form-container">
-      <h1>¡Bienvenido!</h1>
+      <h1 className="title">¡Bienvenido!</h1>
+      <h2 className="subtitle">Accede a tu espacio de aprendizaje</h2>
       <form>
         <TextField
-          label="Correo Electrónico"
+          placeholder="Correo Electrónico"
           variant="outlined"
           fullWidth
           margin="dense"
           type="email"
           required
           className="email-field"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <EmailIcon />
+              </InputAdornment>
+            ),
+          }}
         />
         <TextField
-          label="Contraseña"
+          placeholder="Contraseña"
           variant="outlined"
           fullWidth
           margin="dense"
           type="password"
           className="password-field"
           required
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+          }}
         />
+        <div className="register-btn-container">
+          <Link to="/register" className="register-btn">
+            Solicitar registro
+          </Link>
+        </div>
         <Button variant="contained" type="submit" className="submit-btn">
           Iniciar Sesión
         </Button>
       </form>
-      <p>
-        ¿No tienes una cuenta? <Link to="/register">Regístrate aquí</Link>
-      </p>
     </div>
   );
 };
