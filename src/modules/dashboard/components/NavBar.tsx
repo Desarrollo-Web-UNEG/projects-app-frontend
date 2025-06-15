79b4e60867bf5e1c09d26ecc84b0afbcb0ff2172
Sebastián Landaeta <sebas.landa@hotmail.com>
2025-06-15 12:47:58 -0400

    [ADD] NavBar Responsive
    - El NavBar ahora tiene una vista responsive en la que usa un menú desplegable para mostrar los botones.
    
    - El letterhead ahora tiene una variante responsive.

--- src/modules/dashboard/components/NavBar.tsx
+++ src/modules/dashboard/components/NavBar.tsx
@@ -1,20 +1,52 @@
+import { useState } from "react";
 import { Letterhead } from "@/modules/auth/components";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+import IconButton from "@mui/material/IconButton";
+import Drawer from "@mui/material/Drawer";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import "@dashboard/styles/navbar.css";
 
+// Botones del menú
+const menuButtons = (
+  <>
+    <button>Inicio</button>
+    <button>Proyectos</button>
+    <button>Perfil</button>
+    <button>Cerrar Sesión</button>
+  </>
+);
+
 const NavBar = () => {
-  return (
-    <>
-      <div className="navbar">
-        <Letterhead>
-          <nav>
-            <button>Inicio</button>
-            <button>Proyectos</button>
-            <button>Perfil</button>
-            <button>Cerrar Sesión</button>
-          </nav>
-        </Letterhead>
-      </div>
-    </>
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (state: boolean) => () => setOpen(state);
+
+  return isMobile ? (
+    <div className="navbar">
+      <Letterhead>
+        <IconButton
+          className="menu-button"
+          aria-label="menu"
+          onClick={toggleDrawer(true)}
+        >
+          <DehazeIcon />
+        </IconButton>
+        <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+          <div className="drawer-content" onClick={toggleDrawer(false)}>
+            {menuButtons}
+          </div>
+        </Drawer>
+      </Letterhead>
+    </div>
+  ) : (
+    <div className="navbar">
+      <Letterhead>
+        <nav>{menuButtons}</nav>
+      </Letterhead>
+    </div>
   );
 };
 
