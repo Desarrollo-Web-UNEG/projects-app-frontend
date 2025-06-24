d097beb671beaa2b83478bf689c60eca2d5e239d
Acosta-Saul <srmixter123@gmail.com>
2025-06-24 12:40:07 -0400

    [ADD] conexion con endpoint login y cierre de sesion

---
+++ src/modules/auth/components/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = localStorage.getItem("access_token");
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
